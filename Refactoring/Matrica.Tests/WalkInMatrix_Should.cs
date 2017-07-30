28cffc0ddf6252a4932c5bfc9c816675a42d9eaa
Georgi Markov <markov_ood@abv.bg>
2017-07-31 01:01:48 +0300

    Some initial quick formatting...Removed unnecessary blank lines and added curly brackets(body) for the 'if's and for the loops...

---
+++ Refactoring/Matrica.Tests/WalkInMatrix_Should.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matrica;
+using NUnit.Framework;
+
+namespace Matrica.Tests
+{
+    [TestFixture]
+    public class WalkInMatrix_Should
+    { 
+        [Test]
+        public void IncreaseCellCounterOnEveryStep()
+        {
+            var a = new WalkInMatrix();
+            
+        }
+    }
+}
